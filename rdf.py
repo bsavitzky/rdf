d7bca75f9df10b8053061f1db4f01c64e6f3bef4
bsavitzky <ben.savitzky@gmail.com>
2015-10-03 15:38:17 -0400

    Re-wrote experimental rdf calculation in rdf.py to handle boundary conditions.

--- rdf.py
+++ rdf.py
@@ -7,7 +7,7 @@ Contains all functions which calculate RDFs. All are in 2D.
 
 # Import global libraries
 import numpy as np
-
+import warnings
 
 # Paracrystal fit for square lattice...
 def sq_para(r_data, sigma, mu):
@@ -53,77 +53,110 @@ def sq_para_projection(r_data, sigma, mu, c):
     return g
 
 
-# Calculate experimental RDF from particle centers
-def experimental(x,y,S,rMax,dr):
-    """Compute the two-dimensional pair correlation function, also known
-    as the radial distribution function, for a set of circular particles
-    contained in a square region of a plane.  This simple function finds
-    reference particles such that a circle of radius rMax drawn around the
-    particle will fit entirely within the square, eliminating the need to
-    compensate for edge effects.  If no such particles exist, an error is
-    returned. Try a smaller rMax...or write some code to handle edge effects! ;)
+# Calculate the excess area that must be subtracted off an annulus with inner radius r and
+# outer radius R with a center x from the box edge, where x<r.
+def calc_excess(r, R, x):
+    # Should be one line of code, but np.where appears to be written poorly
+    # Mask and alternate r/R arrays prevent acrcos and sqrt from throwing errors
+    mask = x<r
+    r_mask = np.where(mask, r, x+1)   # (x+1) is a dummy, never appears in final output
+    R_mask = np.where(mask, R, x+1)   #   but ensures arccos and sqrt won't break
+    return np.where(mask, R_mask**2*np.arccos(x/R_mask) - r_mask**2*np.arccos(x/r_mask)
+                        - x*(np.sqrt(R_mask**2-x**2) - np.sqrt(r_mask**2-x**2)), 0)
 
-    Arguments:
-        x               an array of x positions of centers of particles
-        y               an array of y positions of centers of particles
-        S               length of each side of the square region of the plane
-        rMax            outer diameter of largest annulus
-        dr              increment for increasing radius of annulus
 
-    Returns a tuple: (g, radii, interior_x, interior_y)
-        g(r)            a numpy array containing the correlation function g(r)
-        radii           a numpy array containing the radii of the
-                        annuli used to compute g(r)
-        interior_x      x coordinates of reference particles
-        interior_y      y coordinates of reference particles
+# Calculate overlap of x and y excess
+def calc_xy_excess(r, R, x, y):
+    mask = (x<r)*(y<r)  # i.e. and statement
+    r_mask = np.where(mask, r, x+y+1)   # as in calc_excess, (x+y+1) is a dummy variable,
+    R_mask = np.where(mask, R, x+y+1)   #   chosen such that arccos and sqrts are always valid
+    return np.where(mask, 0.5*( R_mask**2*(np.arccos(x/R_mask)+np.arccos(y/R_mask)-np.pi/2)
+                    - r_mask**2*(np.arccos(x/r_mask)+np.arccos(y/r_mask)-np.pi/2)
+                    - x*(np.sqrt(R_mask**2-x**2) - np.sqrt(r_mask**2-x**2))
+                    - y*(np.sqrt(R_mask**2-y**2) - np.sqrt(r_mask**2-y**2)) ), 0)
 
-    Citation - this function pulled from:
-    https://github.com/cfinch/Shocksolution_Examples/tree/master/PairCorrelation
+
+# Find members of a sorted list closest to a given value.
+# Returns values bounding input value
+def nearest_vals_array(arr, vals_ar):
     """
-    from numpy import zeros, sqrt, where, pi, average, arange, histogram
+    Accepts:
+        arr - a sorted array
+        vals_arr - an array of numbers
+    Returns:
+        lower - an array with the shape of vals_ar, populated by the closest element of
+                arr to each value in vals_ar that is less than that value
+        upper - same idea
+    """
+    if vals_ar.min()<arr.min() or vals_ar.max()>arr.max():
+        print "Error: value in vals_ar outside range of arr."
+        return 0,0
+    lower = np.empty_like(vals_ar)
+    upper = np.empty_like(vals_ar)
+    for i in range(len(vals_ar)):
+        ind = (np.abs(arr - vals_ar[i])).argmin()
+        if (arr[ind]-vals_ar[i]) <= 0 :
+            lower[i],upper[i] = arr[ind], arr[ind+1]
+        else:
+            lower[i],upper[i] = arr[ind-1], arr[ind]
+    return lower, upper
+
+
+# Calculate experimental RDF from particle centers
+def experimental(x,y,fov,dr):
+    """
+    Calculates the 2D radial distribution function from experimental data.
+    Handles boundaries by calculating appropriate normalization area of each annulus near edges
+
+    Inputs: x - ndarray of x coordinates of particle centers, in pixels
+            y - ndarray of y coordinates, in pixels
+            fov - float of bounding box side length, in pixels
+            dr - binsize for rdf histogram, in pixels
+    Output: g_average - ndarray of the radial distribution function
+            radii - ndarray of the radius at the center of each bin used to calculate g(r)
+    """
     # Number of particles in ring/area of ring/number of reference particles/number density
     # area of ring = pi*(r_outer**2 - r_inner**2)
+    import warnings
+    warnings.filterwarnings('error')
 
-    # Find particles which are close enough to the box center that a circle of radius
-    # rMax will not cross any edge of the box
-    bools1 = x>1.1*rMax
-    bools2 = x<(S-1.1*rMax)
-    bools3 = y>rMax*1.1
-    bools4 = y<(S-rMax*1.1)
-    interior_indices, = where(bools1*bools2*bools3*bools4)
-    num_interior_particles = len(interior_indices)
-
-    print "Analyzing "+str(num_interior_particles)+" particles out of "+str(len(x))
-
-    if num_interior_particles < 1:
-        raise  RuntimeError ("No particles found for which a circle of radius rMax\
-                will lie entirely within a square of side length S.  Decrease rMax\
-                or increase the size of the square.")
-
-    edges = arange(0., rMax+1.1*dr, dr)
-    num_increments = len(edges)-1
-    g = zeros([num_interior_particles, num_increments])
-    radii = zeros(num_increments)
-    numberDensity = float(len(x))/S**2
+    rbins = np.arange(0., fov, dr)
+    num_increments = len(rbins)
+    g = np.zeros([len(x), num_increments-1])
+    radii = np.zeros(num_increments)
+    numberDensity = float(len(x))/fov**2
 
     # Compute pairwise correlation for each interior particle
-    for p in range(num_interior_particles):
-        index = interior_indices[p]
-        d = sqrt((x[index]-x)**2 + (y[index]-y)**2)
-        d[index] = 2*rMax
+    for i in range(len(x)):
+        print "Analyzing particle {} of {}".format(i, len(x))
+        d = np.sqrt((x[i]-x)**2 + (y[i]-y)**2)
+        d_lower, d_upper = nearest_vals_array(np.arange(0, np.sqrt(2)*fov+dr, dr),d)
+        d[i] = fov
 
-        (result,bins) = histogram(d, bins=edges, normed=False)
-        g[p,:] = result/numberDensity
+        # Determine normalization, if (x[i],y[i]) is near an edge
+        x_e, y_e = min(x[i], fov-x[i]), min(y[i], fov-y[i]) # Find distance to edge
+
+        x_excess = calc_excess(d_lower, d_upper, x_e)
+        y_excess = calc_excess(d_lower, d_upper, y_e)
+        xy_excess = calc_xy_excess(d_lower, d_upper, x_e, y_e)
+
+        annulus_areas = np.pi*( d_upper**2 - d_lower**2 )
+        area_normalization = annulus_areas - x_excess - y_excess + xy_excess
+
+
+        (result,bins) = np.histogram(d, bins=rbins, normed=False, weights=1.0/area_normalization)
+        g[i,:] = result/numberDensity
+
 
     # Average g(r) for all interior particles and compute radii
-    g_average = zeros(num_increments)
-    for i in range(num_increments):
-        radii[i] = (edges[i] + edges[i+1])/2.
-        rOuter = edges[i+1]
-        rInner = edges[i]
-        g_average[i] = average(g[:,i])/(pi*(rOuter**2 - rInner**2))
+    g_average = np.zeros(num_increments)
+    for i in range(num_increments-1):
+        radii[i] = (bins[i] + bins[i+1])/2.
+        g_average[i] = np.average(g[:,i])
 
-    return (g_average, radii, x[interior_indices], y[interior_indices])
+    return (g_average, radii)
+
+
 
 
 if __name__=="__main__":
@@ -131,6 +164,7 @@ if __name__=="__main__":
     import argparse
     import os
     import skimage.io
+    import time
 
     # Import local libraries
     import mdscrape as md
@@ -139,7 +173,6 @@ if __name__=="__main__":
     parser=argparse.ArgumentParser()
     parser.add_argument("image_file")
     parser.add_argument("centroid_file")
-    parser.add_argument("rmax")
     parser.add_argument("dr")
     args=parser.parse_args()
 
@@ -163,7 +196,8 @@ if __name__=="__main__":
 
     # Perform rdf calculations
     print "Done. Calculating experimental RDF."
-    g_exp,r_pixels,x0,y0 = experimental(x,y,fov_pixels,float(args.rmax)*spacing_pixels, float(args.dr)*spacing_pixels)
+    time_init = time.time()
+    g_exp,r_pixels = experimental(x,y,fov_pixels, float(args.dr)*spacing_pixels)
     r_nm = r_pixels / pixels_per_nm  # convert to nm
 
 ###### TODO: extract sigma (also possibly mu, c) properly here....!!! ######
@@ -172,9 +206,12 @@ if __name__=="__main__":
     mu_nm = 6.5
     c = 3
 
-    g_ideal = sq_para(r_pixels, sigma=sigma_nm*pixels_per_nm, mu=mu_nm*pixels_per_nm)
-    g_proj = sq_para_projection(r_pixels, sigma=sigma_nm*pixels_per_nm, mu=mu_nm*pixels_per_nm, c=3)
+#    g_ideal = sq_para(r_pixels, sigma=sigma_nm*pixels_per_nm, mu=mu_nm*pixels_per_nm)
+#    g_proj = sq_para_projection(r_pixels, sigma=sigma_nm*pixels_per_nm, mu=mu_nm*pixels_per_nm, c=3)
+    g_ideal=0
+    g_proj=0
 
+    print "Done. Calculation took {} seconds.".format(time.time()-time_init)
     print "Saving as {}.npz".format(output_name)
     np.savez(output_name, r_nm=r_nm, g_exp=g_exp, g_ideal=g_ideal, g_proj=g_proj, sigma_nm=sigma_nm,mu_nm=mu_nm,c=c)
 
