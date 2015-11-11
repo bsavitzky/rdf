908576388f4283da5915aaf65be779189b80da79
bsavitzky <ben.savitzky@gmail.com>
2015-11-11 15:35:41 -0500

    Cleans up plot_rdf.py

--- plot_rdf.py
+++ plot_rdf.py
@@ -11,27 +11,25 @@ import argparse
 # Deal with I/O
 parser=argparse.ArgumentParser()
 parser.add_argument("rdf_file")
+parser.add_argument("-o","--output",default="outputs/",help="Directory for output files.")
 args=parser.parse_args()
 
-if not os.path.exists("outputs"):
-    os.mkdir("outputs")
-output_name="outputs/"+os.path.splitext(os.path.basename(args.rdf_file))[0]
+if not os.path.exists(args.output):
+    os.mkdir(args.output)
+if args.output[-1]!="/":
+    output_name=args.output+"/"
+else:
+    output_name=args.output
+output_name+=os.path.splitext(os.path.basename(args.rdf_file))[0]
 
 rdf = np.load(args.rdf_file)
 r_nm = rdf['r_nm']
 g_exp = rdf['g_exp']
 g_exp[0]=0
-#g_ideal = rdf['g_ideal']
-#g_proj = rdf['g_proj']
-#sigma_nm = float(rdf['sigma_nm'])
-#mu_nm = float(rdf['mu_nm'])
-#c = int(rdf['c'])
 
-# Plot all_g's together
+# Plot
 fig,ax = plt.subplots()
 ax.plot(r_nm, g_exp,'k-',label=r"Experimental")
-#ax.plot(r_nm, g_ideal, 'r-',label=r"Ideal Para, $\sigma = {}$ nm".format(sigma_nm))
-#ax.plot(r_nm, g_proj, 'y-',label=r"Projection Para, $c = {}$ layers".format(c))
 ax.set_xlabel(r"$r$ (nm)")
 ax.set_ylabel(r"$g(r)$")
 ax.set_title("Radial Distribution Functions")
